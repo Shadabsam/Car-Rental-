9d47d3b82ac43ee6b401cdb689208080babef317
shadabsam <shadabsam91751@gmail.com>
2024-03-02 00:44:21 +0530

    table created for car rental

--- Car_Rental.py
+++ Car_Rental.py
@@ -1,2 +1,31 @@
-a="shadab alam"
-print(a)
+import sqlite3 as sq
+conn=sq.connect("Carbookings.db")
+conn.cursor()
+try:
+    conn.execute("""create table if not exists admin 
+                 (admin_id integer primary key,
+                 admin_username varchar(20) not null,
+                 admin_password varchar(20) not null)""")
+    conn.execute('''create table if not exists car_dealers 
+                 (car_dealarid integer primary key,
+                 car_dealername varchar (20) not null,
+                 car_dealerpassword varchar(20) not null,
+                 car_dealermail varchar(20) not null,
+                 car_dealerphone varchar(20) not null)''')
+    conn.execute('''create table if not exists cars
+                 (car_id integer primary key,
+                 car_name varchar(20) not null,
+                 car_type varchar(20) not null,
+                 car_model varchar(20) not null,
+                 car_dealer int(11) not null,
+                 car_from varchar(20) not null,
+                 car_to varchar(20) not null)''')
+    conn.execute('''create table if not exists users
+                 (user_id integer primary key,
+                 user_name varchar(20) not null,
+                 user_password varchar(20) not null,
+                 user_email varchar(20) not null,
+                 user_phone varchar(20) not null)''')
+    
+except:
+    print("some error")
